8d575b603d139a250d011434645dd9b582228b2a
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-10 14:49:28 +0700

    Fix calculation work
    Calculator should not evaluate more than a single pair of numbers
    at a time. Example: you press a number button (12), followed by an
    operator button (+), a second number button (7), and finally a second
    operator button (-). Your calculator should then do the following:
    first, evaluate the first pair of numbers (12 + 7), second, display
    the result of that calculation (19), and finally, use that result (19)
    as the first number in your new calculation,
    along with the next operator (-).

--- js/scripts.js
+++ js/scripts.js
@@ -39,38 +39,60 @@ function operate(a, b, op) {
   return result;
 }
 
-let numbers = [];
-let operator;
-let display = document.querySelector('.calculator__display');
-const operateBtn = document.querySelector('.calculator__operateBtn');
+function main() {
+  let numbers = [];
+  let operator;
+  let display = document.querySelector('.calculator__display');
+  const operateBtn = document.querySelector('.calculator__operateBtn');
 
-//num listener
-document.querySelectorAll('.calculator__numBtn').forEach((numBtn) => {
-  numBtn.addEventListener('click', () => {
-    if (display.textContent === '0' || Number(display.textContent) === numbers.at(-1)) {
-      display.textContent = '';
-    }
+  //num listener
+  document.querySelectorAll('.calculator__numBtn').forEach((numBtn) => {
+    numBtn.addEventListener('click', () => {
+      if (display.textContent === '0' || Number(display.textContent) === numbers.at(-1)) {
+        display.textContent = '';
+      }
 
-    display.textContent += numBtn.textContent;
+      display.textContent += numBtn.textContent;
+    });
   });
-});
 
-//op listener
-document.querySelectorAll('.calculator__opBtn').forEach((opBtn) => {
-  opBtn.addEventListener('click', () => {
+  //op listener
+  document.querySelectorAll('.calculator__opBtn').forEach((opBtn) => {
+    opBtn.addEventListener('click', () => {
+      if (!(+display.textContent === numbers[0])) {
+        numbers.push(+display.textContent); //check if operate was pressed
+      }
+
+      if (numbers.length >= 2) {
+        let result = operate(numbers.at(-2), numbers.at(-1), operator);
+        display.textContent = result;
+        numbers.push(result);
+      }
+
+      operator = opBtn.textContent;
+
+      console.log(numbers);
+    });
+  });
+
+  operateBtn.addEventListener('click', () => {
     numbers.push(+display.textContent);
-    operator = opBtn.textContent;
+
+    if (numbers.length >= 2) {
+      let result = operate(numbers.at(-2), numbers.at(-1), operator);
+      display.textContent = result;
+      numbers = [];
+      numbers.push(result);
+    }
+    console.log(numbers);
   });
-});
 
-operateBtn.addEventListener('click', () => {
-  numbers.push(+display.textContent);
-  display.textContent = operate(numbers.at(-2), numbers.at(-1), operator);
-});
+  // clear button wipe all
+  document.querySelector('.calculator__clearBtn').addEventListener('click', () => {
+    display.textContent = '0';
+    numbers = [];
+    operator = '';
+  });
+}
 
-// clear button wipe all
-document.querySelector('.calculator__clearBtn').addEventListener('click', () => {
-  display.textContent = '0';
-  numbers = [];
-  operator = '';
-});
+main();
